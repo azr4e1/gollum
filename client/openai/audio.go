cdc8efb7c9f5afecb0f5f2fd84c8801c0fbfe1df
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-10-22 13:04:17 +0100

    feat: added audio structs for openai

--- client/openai/audio.go
+++ client/openai/audio.go
@@ -1,7 +1,18 @@
 package openai
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
 type openaiVoice string
 type audioFormat string
+type audioModel string
+type audioOption func(*audioRequest) error
 
 const (
 	Alloy   openaiVoice = "alloy"
@@ -21,10 +32,94 @@ const (
 	PCM  audioFormat = "pcm"
 )
 
+const (
+	TTS1   audioModel = "tts-1"
+	TTS1HD audioModel = "tts-1-hd"
+)
+
 type audioRequest struct {
-	Model  string      `json:"model"`
+	Model  audioModel  `json:"model"`
 	Input  string      `json:"input"`
 	Voice  openaiVoice `json:"voice"`
 	Format audioFormat `json:"response_format,omitempty"`
-	Speed  *int        `json:"speed,omitempty"`
+	Speed  *float64    `json:"speed,omitempty"`
+}
+
+func WithAudioModel(model audioModel) audioOption {
+	return func(aR *audioRequest) error {
+		aR.Model = model
+		return nil
+	}
+}
+func WithAudioInput(input string) audioOption {
+	return func(aR *audioRequest) error {
+		if input == "" {
+			return errors.New("input is empty.")
+		}
+		aR.Input = input
+		return nil
+	}
+}
+func WithAudioVoice(voice openaiVoice) audioOption {
+	return func(aR *audioRequest) error {
+		aR.Voice = voice
+		return nil
+	}
+}
+func WithAudioFormat(format audioFormat) audioOption {
+	return func(aR *audioRequest) error {
+		aR.Format = format
+		return nil
+	}
+}
+func WithAudioSpeed(speed float64) audioOption {
+	return func(aR *audioRequest) error {
+		if speed < 0.25 || speed > 4 {
+			return errors.New("speed must be between 0.25 and 4. Default is 1.")
+		}
+		aR.Speed = &speed
+		return nil
+	}
+}
+
+func NewAudioRequest(opts ...audioOption) (*audioRequest, error) {
+	request := new(audioRequest)
+	for _, o := range opts {
+		err := o(request)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if request.Model == "" {
+		return nil, errors.New("missing model.")
+	}
+	if request.Voice == "" {
+		return nil, errors.New("missing voice.")
+	}
+	if request.Input == "" {
+		return nil, errors.New("missing input.")
+	}
+
+	return request, nil
+}
+
+func makeHTTPAudioRequest(request *audioRequest, oc openaiClient) (*http.Response, error) {
+	jsonRequest, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, audioURL, bytes.NewReader(jsonRequest))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", oc.apiKey))
+
+	client := http.Client{Timeout: time.Duration(30 * time.Second)}
+	res, err := client.Do(req)
+
+	return res, err
 }
